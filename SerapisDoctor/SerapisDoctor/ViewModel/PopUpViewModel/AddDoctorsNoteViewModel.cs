5259223ba4498fd920bb95fd00363377de294e23
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2019-06-03 22:32:03 +0200

    -added code for doctors adding notes

--- SerapisDoctor/SerapisDoctor/ViewModel/PopUpViewModel/AddDoctorsNoteViewModel.cs
+++ SerapisDoctor/SerapisDoctor/ViewModel/PopUpViewModel/AddDoctorsNoteViewModel.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Services;
+using System.Threading.Tasks;
 
 namespace SerapisDoctor.ViewModel
 {
@@ -14,7 +17,12 @@ namespace SerapisDoctor.ViewModel
         public AddDoctorsNoteViewModel()
         {
             InitalizeDate();
+
+            SaveDoctorsNote = new Command(async () => await SaveDocNoteAsync());
         }
+
+
+        public Command SaveDoctorsNote { get; set; }
 
         private string todaysDate;
         public string  TodaysDate
@@ -31,10 +39,36 @@ namespace SerapisDoctor.ViewModel
             }
         }
 
+
+        private string patientsNote;
+
+        public string PatientsNote
+        {
+            get
+            {
+                return patientsNote;
+            }
+            set
+            {
+                patientsNote = value;
+                OnPropertyChanged("PatientsNote");
+                patientsNote = value;
+            }
+        }
+
+
         private string InitalizeDate()
         {
             //The follwoing converts the month from an interger to human readable dates like march,april etc
             return todaysDate = DateTime.Today.Day + " " + dateConverter.ConvertMonth(DateTime.Today.Month) + " " + DateTime.Today.Year;
+        }
+
+        private async Task SaveDocNoteAsync()
+        {
+            //Closes the pop up and adds the note to the current patient
+
+
+            await Task.FromResult(PopupNavigation.Instance.PopAllAsync(true));
         }
 
     }
