9ac5e4aa308bd53a534bf0bada78933f47316b5c
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2019-06-03 16:31:38 +0200

    added a ScheduleViewModel file

---
+++ SerapisDoctor/SerapisDoctor/ViewModel/TabbedPageViewModels/ScheduleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerapisDoctor.ViewModel.TabbedPageViewModels
+{
+    public class ScheduleViewModel
+    {
+    }
+}
