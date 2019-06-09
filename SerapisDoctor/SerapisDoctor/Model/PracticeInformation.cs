ddb17bc25a83846027e0b4e243dfc44e1e03b028
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2019-06-09 12:13:43 +0200

    Moved the practiceinformation file from the doctor folder to the model folder.

---
+++ SerapisDoctor/SerapisDoctor/Model/PracticeInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerapisDoctor.Model
+{
+    public class PracticeInformation
+    {
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string PracticeName { get; set; }
+
+    }
+}
