015dacc4493d3304c5a6eec7bfa0fd1711a9d860
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2019-06-09 12:11:08 +0200

    Added new files in the Services folder and the model folder

---
+++ SerapisDoctor/SerapisDoctor/Model/Doctor/PracticeInformation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerapisDoctor.Model.Doctor
+{
+    public class PracticeInformation
+    {
+    }
+}
---
+++ SerapisDoctor/SerapisDoctor/Services/DoctorLocationTracker.cs
@@ -0,0 +1,27 @@
+﻿using SerapisDoctor.Model.Doctor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace SerapisDoctor.Services
+{
+    public class DoctorLocationTracker
+    {
+        public DoctorLocationTracker()
+        {
+            
+        }
+
+        private void GetDoctorsCurrentLocation()
+        {
+            Geolocation.GetLocationAsync();
+        }
+
+        //Calculate from the gps co-ordinates where the doctor possibly is at
+        private void GetPossibleCurrentPractice(List<DoctorUser>)
+        {
+
+        }
+    }
+}
